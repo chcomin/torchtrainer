a8904f084dee13d70e21129bc23a12ff1e9610b9
Cesar Henrique Comin <chcomin@gmail.com>
2021-04-24 15:45:07 -0300

    Function parameter annotation

---
+++ torchtrainer/util.py
@@ -0,0 +1,24 @@
+import inspect
+
+def save_params(store):
+    """Annotator for saving function parameters."""
+
+    def func_caller(wrapped_func):
+        signature = inspect.signature(wrapped_func)
+        params = signature.parameters
+        for param_name in params:
+            param = params[param_name]
+            store[param_name] = param.default
+            
+        def func(*args, **kwargs):
+            for idx, param_name in enumerate(store):
+                if idx<len(args):
+                    store[param_name] = args[idx]
+                else:
+                    if param_name in kwargs:
+                        store[param_name] = kwargs[param_name]
+                
+            wrapped_func(*args, **kwargs)
+            
+        return func
+    return func_caller
